d279d635d8c73038a40f07a66643843f6df48370
irfanpayok <irfan.reza@selleri.id>
2022-05-16 10:57:37 +0700

    integrate api, implement awards list and snippets for filter

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,151 +1,79 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
+  <div>
+   <v-container fluid grid-list-lg>
+      <v-layout row wrap>
+        <v-flex xs12 v-for="award in awards" :key="award.id">
+        <v-lazy
+          v-model="isActive"
+          :options="{
+            threshold: .5
+          }"
+          min-height="200"
+          transition="fade-transition"
+        >
+          <v-card
+            class="mx-auto"
+            max-width="400"
+            height="200px"
           >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
+            <v-img
+              :src="`${award.image}`"
+              aspect-ratio="2.75"
+              class="white--text align-end"
+            >
+                <v-chip class="ma-2" :color="`${award.type === 'Vouchers' ? 'primary' : 'green'}`" text-color="white" :style="{ padding: '14px', float: 'right' }">
+                  {{award.type}}
+                </v-chip>
+                <v-card-title :style="{}" >{{award.exchange_point.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}} Poin</v-card-title>
+            </v-img>
+            <div :style="{ padding: '15px', fontWeight: 'bold' }">
+              {{award.name}}
+            </div>
+          </v-card>
+        </v-lazy>
+        </v-flex>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+      </v-layout>
+    </v-container>
+    <div v-if="awards.length" v-observe-visibility="handleScrolledToBottom"></div>
+  </div>
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
-    name: 'HelloWorld',
+    name: 'Award',
 
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+      awards: [],
+      page: 1,
+      limit: 5,
+      lastPage: 1,
+      isActive: false,
     }),
+
+    methods: {
+      async fetch () {
+        const config = { headers: { 'Content-Type': 'application/json' } }
+        let awards = await axios.post(`http://localhost:3030/api/v1/awards?page=${this.page}&limit=${this.limit}`, {}, config)
+
+        this.awards.push(...awards.data.data)
+        this.lastPage = awards.data.meta.last_page
+      },
+
+      handleScrolledToBottom (isVisible) {
+        if (!isVisible) { return }
+        if (this.page >= this.lastPage) { return }
+
+        this.page++
+
+        this.fetch()
+      }
+    },
+
+    mounted () {
+      this.fetch()
+    }
   }
 </script>
